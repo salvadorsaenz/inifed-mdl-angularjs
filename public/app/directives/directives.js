d8cc660fe09fdd515a478780915b0df4d885bdb2
Salvador Sáenz <salvador_saenz@hotmail.com>
2017-12-22 12:17:11 -0600

    Envío de reporte

--- public/app/directives/directives.js
+++ public/app/directives/directives.js
@@ -34,20 +34,21 @@ app.directive('ngEnter', function() {
 app.directive('numbersOnly', function() {
     return {
         require: 'ngModel',
-        link: function(scope, element, attr, ngModelCtrl) {
-            function fromUser(text) {
-                if (text) {
-                    var transformedInput = text.replace(/[^0-9]/g, '');
-
-                    if (transformedInput !== text) {
-                        ngModelCtrl.$setViewValue(transformedInput);
-                        ngModelCtrl.$render();
-                    }
-                    return transformedInput;
+        link: function(scope, element, attrs, modelCtrl) {
+            modelCtrl.$parsers.push(function(inputValue) {
+                // this next if is necessary for when using ng-required on your input. 
+                // In such cases, when a letter is typed first, this parser will be called
+                // again, and the 2nd time, the value will be undefined
+                if (inputValue == undefined)
+                    return ''
+                var transformedInput = inputValue.replace(/[^0-9.]/g, '');
+                if (transformedInput != inputValue) {
+                    modelCtrl.$setViewValue(transformedInput);
+                    modelCtrl.$render();
                 }
-                return undefined;
-            }
-            ngModelCtrl.$parsers.push(fromUser);
+
+                return transformedInput;
+            });
         }
     };
 });
--- public/app/modules/obra/obraCtrl.js
+++ public/app/modules/obra/obraCtrl.js
@@ -1,8 +1,8 @@
 
 app.controller("obraCtrl", obraCtrl);
 
-function obraCtrl($scope, environment, consultarPost, consultarGet, appServices, appModelServ, 
-    postNtfFiles, $log, $location) {
+function obraCtrl($scope, environment, consultarPost, consultarGet, appServices, appModelServ,
+        postNtfFiles, $log, $location) {
 
     $log.info('obraCtrl');
     var imagenActual = {
@@ -12,16 +12,23 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
     var dialog = document.querySelector('dialog');
     $scope.user = appModelServ.user;
     $scope.VISTAS = appModelServ.VISTAS;
-    $scope.reporte = {
-        avanceSemanal: 0,
-        textoAvance: '',
-        imagenesAdjuntasSelected: [false, false, false, false, false, false],
-        imagenesAdjuntas: []
+    var newReporte = function() {
+        return {
+            avanceSemanal: 0,
+            textoAvance: '',
+            imagenesAdjuntasSelected: [false, false, false, false, false, false],
+            imagenesAdjuntas: []
+        };
     };
+    $scope.reporte = newReporte();
     $scope.imagenes = {
         porSubir: 0,
         respondioService: 0,
         cargadas: 0
+    };
+    $scope.dialog = {
+        status: 'Reporte enviado correctamente',
+        mensaje: 'Folio: B20420'
     };
 
     $scope.muestraOcultaInfoObra = function() {
@@ -77,6 +84,20 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
         appModelServ.VISTAS['imagenes'].listado.tareaSelected = false;
     };
 
+    var muestraDatosModal = function() {
+        if (appModelServ.VISTAS['imagenes'].selected) {
+            $scope.dialog.status = 'Carga exitosa';
+            $scope.dialog.mensaje = 'Se añadieron ' + $scope.imagenes.cargadas
+                    + ' imágenes a '
+                    + appModelServ.VISTAS['imagenes'].listado.tareaSeleccionada.orden + ' '
+                    + appModelServ.VISTAS['imagenes'].listado.tareaSeleccionada.nombre;
+        } else {
+            $scope.dialog.status = 'Reporte enviado correctamente';
+            $scope.dialog.mensaje = 'Folio: ';
+        }
+        dialog.showModal();
+    };
+
     $scope.guardar = function() {
         $log.info('guardar');
         $scope.imagenes = {
@@ -86,7 +107,7 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
         };
         var files = $('.archivo');
         var arrFiles = [];
-        for (var i=0; i < files.length; i++) {
+        for (var i = 0; i < files.length; i++) {
             if (files[i].files.length > 0) {
 //                $log.info('file', files[i].files[0]);
                 arrFiles.push(files[i].files[0]);
@@ -94,10 +115,10 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
         }
         $log.info('arrFiles', arrFiles);
         $scope.imagenes.porSubir = arrFiles.length;
-        
+
         if ($scope.imagenes.porSubir > 0) {
             var promise = {};
-            for (var j=0; j < arrFiles.length; j++) {
+            for (var j = 0; j < arrFiles.length; j++) {
                 if (environment.getEnvironment === 'SALVADOR') {
                     var serviceItem = {
                         use: 'json'
@@ -114,31 +135,31 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
                 }
 
                 promise.then(
-                    function(payload) {
-                        var result = payload.data;
-                        $log.info('result', result);
-                        if (result.success) {
-                            $scope.imagenes.cargadas++;
-                        }
-                        $scope.imagenes.respondioService++;
+                        function(payload) {
+                            var result = payload.data;
+                            $log.info('result', result);
+                            if (result.success) {
+                                $scope.imagenes.cargadas++;
+                            }
+                            $scope.imagenes.respondioService++;
 
-                        if ($scope.imagenes.respondioService === $scope.imagenes.porSubir) {
-                            dialog.showModal();
-                        }
-                    },
-                    function(errorPayload) {
-                        $log.error('failure errorPayload', errorPayload);
-                        $scope.imagenes.respondioService++;
+                            if ($scope.imagenes.respondioService === $scope.imagenes.porSubir) {
+                                muestraDatosModal();
+                            }
+                        },
+                        function(errorPayload) {
+                            $log.error('failure errorPayload', errorPayload);
+                            $scope.imagenes.respondioService++;
 
-                        if ($scope.imagenes.respondioService === $scope.imagenes.porSubir) {
-                            dialog.showModal();
-                        }
-                    });
+                            if ($scope.imagenes.respondioService === $scope.imagenes.porSubir) {
+                                muestraDatosModal();
+                            }
+                        });
             }
         }
-        
+
     };
-    
+
     var consultaImagenesPorTarea = function() {
         var promise = {};
         if (environment.getEnvironment === 'SALVADOR') {
@@ -159,8 +180,8 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
                     if (result.success) {
                         appModelServ.VISTAS['reportes'].cargar.imagenesPorTarea = result.data;
                         appModelServ.setVariableSesion('VISTAS', appModelServ.VISTAS);
-                        
-                        for (var i=0; i < result.data.length; i++) {
+
+                        for (var i = 0; i < result.data.length; i++) {
                             appModelServ.VISTAS['reportes'].cargar.imagenes[i].imagencargada_idImagen = result.data[i].idImagen;
                             appModelServ.VISTAS['reportes'].cargar.imagenes[i].imagencargada_base64 = result.data[i].imagen;
                             appModelServ.VISTAS['reportes'].cargar.imagenes[i].imagencargada_nombre = result.data[i].nombre;
@@ -173,16 +194,16 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
                     $log.error('error consultaImagenesPorTarea', errorPayload);
                 });
     };
-    
+
     $scope.agregarImagenes = function() {
         appModelServ.VISTAS['reportes'].cargar.reporteSelected = false;
         appModelServ.VISTAS['reportes'].cargar.imagenesSelected = true;
         consultaImagenesPorTarea();
     };
-    
+
     $scope.adjuntar = function() {
         $scope.reporte.imagenesAdjuntas = [];
-        for (var i=0; i < $scope.reporte.imagenesAdjuntasSelected.length; i++) {
+        for (var i = 0; i < $scope.reporte.imagenesAdjuntasSelected.length; i++) {
             if ($scope.reporte.imagenesAdjuntasSelected[i]) {
                 $scope.reporte.imagenesAdjuntas.push(appModelServ.VISTAS['reportes'].cargar.imagenes[i].imagencargada_idImagen);
             }
@@ -191,9 +212,42 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
         appModelServ.VISTAS['reportes'].cargar.reporteSelected = true;
         appModelServ.VISTAS['reportes'].cargar.imagenesSelected = false;
     };
-    
+
     $scope.enviarReporte = function() {
-        
+        var data = {
+            idSemana: appModelServ.VISTAS['reportes'].listado.tareaSeleccionada.idSemana,
+            idTarea: appModelServ.VISTAS['reportes'].listado.tareaSeleccionada.idTarea,
+            porcentaje: $scope.reporte.avanceSemanal,
+            porcentajeAculado: appModelServ.VISTAS['reportes'].listado.tareaSeleccionada.porcentajeAcumulado,
+            descripcion: $scope.reporte.textoAvance,
+            usuario: appModelServ.user.idUsuario,
+            imagenes: $scope.reporte.imagenesAdjuntas
+        };
+        $log.info('enviarReporte data', data);
+        var promise = {};
+        if (environment.getEnvironment === 'SALVADOR') {
+            var serviceItem = {
+                use: 'json'
+                , json: 'public/assets/json/EnviaReporte_response.json'
+            };
+            promise = appServices.exec(serviceItem);
+        } else {
+            // CambiarNombreServicio
+            promise = consultarPost(data, 'residente/reporte/tarea');
+        }
+
+        promise.then(
+                function(payload) {
+                    var result = payload.data;
+                    if (result.success) {
+                        $scope.reporte = newReporte();
+                        muestraDatosModal();
+                    }
+                },
+                function(errorPayload) {
+                    $log.error('error consultaObra', errorPayload);
+                    muestraDatosModal();
+                });
     };
 
     var consultaObra = function() {
@@ -291,25 +345,34 @@ function obraCtrl($scope, environment, consultarPost, consultarGet, appServices,
             };
         }
     };
-    
+
     var regresarAHome = function() {
-        $('.archivo').val('');
+        if (appModelServ.VISTAS['imagenes'].selected) {
+            appModelServ.VISTAS['imagenes'].listado.selected = true;
+            appModelServ.VISTAS['imagenes'].cargar.selected = false;
+            appModelServ.VISTAS['imagenes'].listado.etapaSelected = true;
+            appModelServ.VISTAS['imagenes'].listado.tareaSelected = false;
+            appModelServ.VISTAS['imagenes'].cargar.imagenes = appModelServ.getNewViewImagenes();
+        } else {
+            appModelServ.VISTAS['reportes'].listado.selected = true;
+            appModelServ.VISTAS['reportes'].cargar.selected = false;
+            appModelServ.VISTAS['reportes'].listado.etapaSelected = true;
+            appModelServ.VISTAS['reportes'].listado.tareaSelected = false;
+            appModelServ.VISTAS['reportes'].cargar.imagenes = appModelServ.getNewViewImagenes();
+        }
         appModelServ.VISTAS['home'].selected = true;
         appModelServ.VISTAS['imagenes'].selected = false;
-        appModelServ.VISTAS['imagenes'].listado.selected = true;
-        appModelServ.VISTAS['imagenes'].cargar.selected = false;
-        appModelServ.VISTAS['imagenes'].listado.etapaSelected = true;
-        appModelServ.VISTAS['imagenes'].listado.tareaSelected = false;
-        appModelServ.VISTAS['imagenes'].cargar.imagenes = appModelServ.getNewViewImagenes();
+        appModelServ.VISTAS['reportes'].selected = false;
+
         $scope.$apply();
     };
-    
+
     var init = function() {
         $('.archivo').on('change', function() {
             uploadfilesChange(this, resultBase64);
         });
-        
-        if (! dialog.showModal) {
+
+        if (!dialog.showModal) {
             dialogPolyfill.registerDialog(dialog);
         }
         $('.close').on('click', function() {
